c13f0518d690f7861ab2235b8e36cdc70d14167b
Eltruadvino <truadvino144@gmai.com>
2024-09-12 00:27:44 -0600

    tarea 3 completada

---
+++ Main.java
@@ -0,0 +1,47 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //sección uno "libro favorito"
+
+        String titulo = "El princito";
+        String autor = "Chespirito";
+        String anoPublicacion = "1999";
+        String genero = "terror";
+        String numeroPaginas = "169";
+
+        System.out.println(titulo);
+        System.out.println(autor);
+        System.out.println(anoPublicacion);
+        System.out.println(genero);
+        System.out.println(numeroPaginas);
+
+        //sección dos "pais que deseas visitar"
+        String nombrePais = "Perú";
+        String capital = "Lima";
+        String idiomaOficial = "Español";
+        String poblacionMillones = "34.05";
+        String moneda = "Soles";
+
+        System.out.println(nombrePais);
+        System.out.println(capital);
+        System.out.println(idiomaOficial);
+        System.out.println(poblacionMillones);
+        System.out.println(moneda);
+
+        //sección tres informacion de un personaje histórico favorito
+
+        String nombreCompleto = "Hassan Emilio";
+        String fechaNacimiento = "08/11/1996";
+        String nacionalidad = "Mexicano";
+        String ocipacion = "Musica";
+        String logroDestacado = "Romper Records";
+
+        System.out.println(nombreCompleto);
+        System.out.println(fechaNacimiento);
+        System.out.println(nacionalidad);
+        System.out.println(ocipacion);
+        System.out.println(logroDestacado);
+
+    }
+}
