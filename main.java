3238e257db5e86d12575271cfabb264c0199caf9
Eltruadvino <truadvino144@gmai.com>
2024-09-16 21:32:34 -0600

    tarea 6 realizada

--- main.java
+++ main.java
@@ -1,23 +1,68 @@
-public class main {
+import java.util.Scanner;
+
+public class Main {
+
     public static void main(String[] args) {
-        // Operación 1
-        double numeradorOperacionUno = (12 + 8) * (5 - 3) - (6 / 2);
-        double denominadorOperacionUno = (3 * 4) + (8 / 2);
-        double resultadoOperacionUno = numeradorOperacionUno / denominadorOperacionUno;
-        System.out.println("Resultado de la Operación 1: " + resultadoOperacionUno);
+        Scanner sc = new Scanner(System.in);
 
-        // Operación 2
-        double numeradorOperacionDos = (((9 * 5) - (15 + 3) + (8 * 2) - (12 / 4)) * (7 + 2) - ((6 * 3) / 2));
-        numeradorOperacionDos = numeradorOperacionDos / (2 + ((3 * 2) / (4 - 2))) + (5 * ((8 / 4) + 1)) - (6 / (2 + 1));
-        double denominadorOperacionDos = (((4 + 2) * (7 - 1)) / (3 + 1)) + (((3 * 2) - (5 + 1)) * (8 / 2));
-        denominadorOperacionDos = denominadorOperacionDos / (3 + ((6 / 2) + (7 / 2))) + (2 * (3 + (8 / 4)));
-        double resultadoOperacionDos = numeradorOperacionDos / denominadorOperacionDos;
-        System.out.println("Resultado de la Operación 2: " + resultadoOperacionDos);
+        // Variables para almacenar los nombres, cantidades, precios, descuentos e impuestos
+        String[] nombres = new String[3];
+        int[] cantidades = new int[3];
+        double[] preciosUnitarios = new double[3];
+        double[] descuentos = new double[3];
+        double[] impuestos = new double[3];
 
-        // Operación 3
-        double numeradorOperacionTres = ((6 + 4) * 3) - (18 / 2);
-        double denominadorOperacionTres = (8 - 5) + (3 * 2);
-        double resultadoOperacionTres = numeradorOperacionTres / denominadorOperacionTres;
-        System.out.println("Resultado de la Operación 3: " + resultadoOperacionTres);
+        // Entrada de usuario
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Ingrese el nombre del producto " + (i + 1) + ": ");
+            nombres[i] = sc.nextLine();
+            System.out.println("Ingrese la cantidad del producto " + (i + 1) + ": ");
+            cantidades[i] = sc.nextInt();
+            System.out.println("Ingrese el precio unitario del producto " + (i + 1) + ": ");
+            preciosUnitarios[i] = sc.nextDouble();
+            System.out.println("Ingrese el porcentaje de descuento para el producto " + (i + 1) + ": ");
+            descuentos[i] = sc.nextDouble();
+            System.out.println("Ingrese el porcentaje de impuestos aplicables para el producto " + (i + 1) + ": ");
+            impuestos[i] = sc.nextDouble();
+            sc.nextLine(); // Limpiar el buffer
+        }
+
+        double totalAcumulado = 0;
+
+        // Cálculos y salida
+        for (int i = 0; i < 3; i++) {
+            // Precio total sin descuento
+            double precioTotalSinDescuento = cantidades[i] * preciosUnitarios[i];
+
+            // Monto descontado
+            double montoDescontado = precioTotalSinDescuento * (descuentos[i] / 100);
+
+            // Precio total con descuento
+            double precioConDescuento = precioTotalSinDescuento - montoDescontado;
+
+            // Total de impuestos aplicados
+            double totalImpuestos = precioConDescuento * (impuestos[i] / 100);
+
+            // Precio total a pagar
+            double precioTotalAPagar = precioConDescuento + totalImpuestos;
+
+            // Sumar al total acumulado
+            totalAcumulado += precioTotalAPagar;
+
+            // Mostrar resultados por producto
+            System.out.println("Producto: " + nombres[i]);
+            System.out.println("Cantidad: " + cantidades[i]);
+            System.out.println("Precio total sin descuento: " + precioTotalSinDescuento);
+            System.out.println("Monto descontado: " + montoDescontado);
+            System.out.println("Precio con descuento: " + precioConDescuento);
+            System.out.println("Impuestos aplicados: " + totalImpuestos);
+            System.out.println("Precio total a pagar: " + precioTotalAPagar);
+            System.out.println("---------------------------------");
+        }
+
+        // Mostrar el total acumulado a pagar por los tres productos
+        System.out.println("Total acumulado a pagar por los tres productos: " + totalAcumulado);
+
+        sc.close();
     }
 }
